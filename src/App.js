bed44bf44abfbcd851f54627335052b50d90f265
jesusxambro <jesus.ambrocio@outlook.com>
2022-03-18 12:35:50 -0500

    added directory component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './categories.styles.scss'
-import CategoryItem from "./components/category-item/category-item.component";
+
+import Directory from './components/directory/directory.component';
+
 const App = () => {
 
     const categories = [
@@ -30,13 +31,7 @@ const App = () => {
         }
     ]
 
-        return (
-            <div className="categories-container">
-                {categories.map((category) => (
-                    <CategoryItem key={category.id} category={category}/> //why is category, category?
-                    ))}
-            </div>
-        );
+        return <Directory categories={categories}/>
 };
 
 export default App;
