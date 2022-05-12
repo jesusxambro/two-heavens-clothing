ba574a8b4dda9a579a022c04603eca1b90e06074
Jesus <jesus.ambrocio@outlook.com>
2022-05-12 12:52:11 -0500

    Added pop up for Google Sign in

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,5 @@
 import { initializeApp} from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
 
 
 const firebaseConfig = {
@@ -12,4 +13,13 @@ const firebaseConfig = {
 
 
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
 
