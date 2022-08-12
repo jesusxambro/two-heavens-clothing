536d17a68dd0fbf94d48737307c99b4888f4d71f
Jesus <jesus.ambrocio@outlook.com>
2022-08-12 10:45:26 -0500

    Added more styling

--- src/components/sign-up-form/sing-up-form.component.jsx
+++ src/components/sign-up-form/sing-up-form.component.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { createAuthUserWithEmailAndPassword , createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
 
 const defaultFormFields = {
   displayName: "",
@@ -14,39 +18,34 @@ const SignUpForm = () => {
 
   console.log(formFields);
 
-  const resetFormFields = () =>{
+  const resetFormFields = () => {
     setFormFields(defaultFormFields);
-  }
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if(password !== confirmPassword) {
+    if (password !== confirmPassword) {
       alert("Passwords do not match!");
       return;
     }
-      try{
-        const { user } = await createAuthUserWithEmailAndPassword(
-          email,
-          password
-        );
-        await createUserDocumentFromAuth(user, {displayName});
-        resetFormFields();
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
 
-        // console.log(response);
-
-      }catch(error){
-        if(error.code === 'auth/email-already-in-use'){
-          alert('Cannot create user, email already in use');
-          console.log()
-        }else{
-          console.log('error creating the user', error);
-        }
-
+      // console.log(response);
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email already in use");
+        console.log();
+      } else {
+        console.log("error creating the user", error);
       }
-
-
-
+    }
   }; //test
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -58,8 +57,8 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up with your email and password.</h1>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text"
           required
           onChange={handleChange}
@@ -67,8 +66,8 @@ const SignUpForm = () => {
           value={displayName}
         />
 
-        <label>Email</label>
-        <input
+        <FormInput
+          label="Email"
           type="email"
           required
           onChange={handleChange}
@@ -76,8 +75,8 @@ const SignUpForm = () => {
           value={email}
         />
 
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           required
           onChange={handleChange}
@@ -85,8 +84,8 @@ const SignUpForm = () => {
           value={password}
         />
 
-        <label>Confirm Password</label>
-        <input
+        <FormInput
+          label="Confirm Password"
           type="password"
           required
           onChange={handleChange}
